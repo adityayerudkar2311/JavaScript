ec20d412b326530e6ca3ada168d9cfead92bfb5d
Aditya Yerudkar <127290435+adityayerudkar2311@users.noreply.github.com>
2024-03-31 05:56:14 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = 33
+
+console.log(typeof score);
+console.log(typeof(score));
+
+/*notes
+"33"=> 33
+"33abc" => NaN
+true => 1; false => 0
+
+*/
+
+let isLoggedIn = 0
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+1 => true; 0 => false
+"" => false
+"Aditya" => true
+
+*/
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
